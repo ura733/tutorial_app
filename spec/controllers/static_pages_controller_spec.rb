f66478cd4f96b415a0f7e9b83f5008275c7b0cea
kosuke <depths.ptk@gmail.com>
2018-11-19 02:56:16 +0900

    add static pages

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+render_views
+
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
+  describe 'Static Page' do
+    describe 'Home Page' do
+      it 'home response successflly' do
+        get :home
+        expect(response).to have_http_status(:success)
+        assert_select "title", "Home | #{base_title}"
+      end
+    end
+
+    describe 'Help Page' do
+      it 'help response successflly' do
+        get :help
+        expect(response).to have_http_status(:success)
+        assert_select "title", "Help | #{base_title}"
+      end
+    end
+
+    describe 'About Page' do
+      it 'about response successflly' do
+        get :about
+        expect(response).to have_http_status(:success)
+        assert_select "title", "About | #{base_title}"
+      end
+    end
+
+  end
+end
